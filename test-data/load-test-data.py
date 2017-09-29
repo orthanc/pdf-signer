6a4409ff93f3f95e25ca248c8902d8df72bbc1e1
Ed Costello <edc.open@gmail.com>
2017-09-29 21:51:12 +1300

    Move test data loaders to use file rather than playbook for creds

--- test-data/load-test-data.py
+++ test-data/load-test-data.py
@@ -1,22 +1,39 @@
 #! /usr/bin/python
-import boto3, iso8601, json, os, pprint
+import boto3, ConfigParser, iso8601, json, os, pprint, sys
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
-pdf_bucket_info = os.environ['PDF_BUCKET'].split(':', 2)
-pdf_storage_class = pdf_bucket_info[2]
-signing_events_topic_info = os.environ['SIGNING_EVENTS_TOPIC'].split(':', 1)
+aws_config = ConfigParser.ConfigParser()
+aws_config.read(script_dir + '/creds/' + sys.argv[1] + '/test_user-aws_services.ini')
 
-pdf_bucket = boto3.session.Session(region_name=pdf_bucket_info[0]) \
-    .resource('s3').Bucket(pdf_bucket_info[1])
-signing_events_topic = boto3.session.Session(region_name=signing_events_topic_info[0]) \
-    .resource('sns').Topic(signing_events_topic_info[1])
+aws_creds = {
+    'aws_access_key_id': aws_config.get('DEFAULT', 'aws_access_key'),
+    'aws_secret_access_key': aws_config.get('DEFAULT', 'aws_secret_key'),
+}
+
+pdf_bucket_info = aws_config.get('DEFAULT', 'pdf_bucket').split(':', 2)
+pdf_storage_class = pdf_bucket_info[2]
+signing_events_topic_info = aws_config.get('DEFAULT', 'signing_events_topic').split(':', 1)
+
+pdf_bucket = boto3.session.Session(
+        region_name=pdf_bucket_info[0],
+        **aws_creds
+    ).resource('s3').Bucket(pdf_bucket_info[1])
+signing_events_topic = boto3.session.Session(
+        region_name=signing_events_topic_info[0],
+        **aws_creds
+    ).resource('sns').Topic(signing_events_topic_info[1])
 
 
 pdf_bucket.upload_file(script_dir + '/s3-initial-data/template.pdf', 'templates/template.pdf', {
         "ServerSideEncryption": "AES256",
         "StorageClass": pdf_storage_class
     })
+pdf_bucket.upload_file(script_dir + '/s3-initial-data/template.txt', 'templates/template.txt', {
+        "ServerSideEncryption": "AES256",
+        "StorageClass": pdf_storage_class
+    })
+
 
 subscribe_result = signing_events_topic.subscribe(
     Protocol='email',
--- test-data/send-test-message.py
+++ test-data/send-test-message.py
@@ -1,10 +1,22 @@
 #! /usr/bin/python
-import boto3, iso8601, json, os, pprint
+import boto3, ConfigParser, iso8601, json, os, pprint, sys
 
-transaction_queue_info = os.environ['TRANSACTION_QUEUE'].split(':', 1)
+script_dir = os.path.dirname(os.path.realpath(__file__))
 
-transaction_queue = boto3.session.Session(region_name=transaction_queue_info[0]) \
-    .resource('sqs').get_queue_by_name(QueueName=transaction_queue_info[1])
+aws_config = ConfigParser.ConfigParser()
+aws_config.read(script_dir + '/creds/' + sys.argv[1] + '/test_user-aws_services.ini')
+
+aws_creds = {
+    'aws_access_key_id': aws_config.get('DEFAULT', 'aws_access_key'),
+    'aws_secret_access_key': aws_config.get('DEFAULT', 'aws_secret_key'),
+}
+
+transaction_queue_info = aws_config.get('DEFAULT', 'transaction_queue').split(':', 1)
+
+transaction_queue = boto3.session.Session(
+        region_name=transaction_queue_info[0],
+        **aws_creds
+    ).resource('sqs').get_queue_by_name(QueueName=transaction_queue_info[1])
 
 transaction_queue.send_message(
     MessageBody='{"amount": 9999, "date": "2017-06-12", "template_key": "templates/template.pdf", "result_key": "results/result.pdf"}'
